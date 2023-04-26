cace58ae752a3b0c4b83f1750ee0468d5cb64060
SantiSanGal <santiagon.g.m@gmail.com>
2023-04-26 17:08:36 -0400

    tercer_commit

--- BlogApp/BlogApp/settings.py
+++ BlogApp/BlogApp/settings.py
@@ -133,5 +133,5 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
--- BlogApp/blog/urls.py
+++ BlogApp/blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import post_list, post_detail
+from .views import post_list, post_detail, PostListView, post_share
 app_name = "blog"
 
 urlpatterns = [
     #se enruta la vista
-    path("", post_list, name="post_list"),
+    #path("", post_list, name="post_list"), <-- cuando se hace por función de vista
+    path("", PostListView.as_view(), name="post_list"),
     #path("<int:id>/", post_detail, name="post_detail")
     #año/mes/día/slug es lo que se pasa como parámetro a la función post_detail, post_detail es de la vista
-
-    path("<int:year>/<int:month>/<int:day>/<slug:post>", post_detail, name="post_detail")
+    path("<int:year>/<int:month>/<int:day>/<slug:post>", post_detail, name="post_detail"),
+    path("<int:post_id>/", post_share, name="post_share")
 ]
